e4be8e12a568821ff7c1465199b02b686877331a
gonzs <gonzalosisnero@gmail.com>
2019-11-28 15:34:11 -0300

    .

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -23,7 +23,7 @@ sap.ui.define([
 		onShowHello : function (){
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg",[sRecipient]);
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			MessageToast.show(sMsg);
 		}
 		});
